b825fa9173aed920dda05751c839339b02c91715
Robert Voigtlaender <r.voigtlaender@gmail.com>
2022-03-09 21:08:25 +0100

    -

--- install/robo_brain/share/robo_brain/robo_brain.py
+++ install/robo_brain/share/robo_brain/robo_brain.py
@@ -92,23 +92,21 @@ def generate_launch_description():
                         'publish_tf': publish_tf,
                          'base_frame': 'base_footprint'}]),
 
-        """
-        Node(
-            package='rf2o_laser_odometry',
-            executable='rf2o_laser_odometry_node',
-            name='rf2o_laser_odometry',
-            output='screen',
-            parameters=[{
-                    'laser_scan_topic': '/scan',
-                    'odom_topic': '/odom',
-                    'publish_tf': True,
-                    'base_frame_id': 'base_footprint',
-                    'odom_frame_id': 'odom',
-                    'init_pose_from_topic': '',
-                    'freq': 40.0}],
-            arguments=['--ros-args', '--log-level', 'ERROR']
-        ),
-        """
+        #Node(
+        #    package='rf2o_laser_odometry',
+        #    executable='rf2o_laser_odometry_node',
+        #    name='rf2o_laser_odometry',
+        #    output='screen',
+        #    parameters=[{
+        #            'laser_scan_topic': '/scan',
+        #            'odom_topic': '/odom',
+        #            'publish_tf': True,
+        #            'base_frame_id': 'base_footprint',
+        #            'odom_frame_id': 'odom',
+        #            'init_pose_from_topic': '',
+        #            'freq': 40.0}],
+        #    arguments=['--ros-args', '--log-level', 'ERROR']
+        #),
 
         Node(
             package='robot_state_publisher',
