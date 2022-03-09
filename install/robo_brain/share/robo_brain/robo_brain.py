1d4e44c47b8fabcba46277cb058726087d7dde20
Robert Voigtlaender <r.voigtlaender@gmail.com>
2022-03-09 21:00:28 +0100

    -

--- install/robo_brain/share/robo_brain/robo_brain.py
+++ install/robo_brain/share/robo_brain/robo_brain.py
@@ -105,7 +105,7 @@ def generate_launch_description():
                     'base_frame_id': 'base_footprint',
                     'odom_frame_id': 'odom',
                     'init_pose_from_topic': '',
-                    'freq': 20.0}],
+                    'freq': 40.0}],
             arguments=['--ros-args', '--log-level', 'ERROR']
         ),
 #"""
