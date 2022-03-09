632c3f772d35ec90c874dfb1212dccf319337cf0
Robert Voigtlaender <r.voigtlaender@gmail.com>
2022-03-09 18:22:08 +0100

    added odom

--- install/robo_brain/share/robo_brain/robo_brain.py
+++ install/robo_brain/share/robo_brain/robo_brain.py
@@ -11,21 +11,23 @@ import xacro
 
 def generate_launch_description():
     serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
-    serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200') #for A1/A2 is 115200
+    serial_baudrate = LaunchConfiguration(
+        'serial_baudrate', default='115200')  # for A1/A2 is 115200
     frame_id = LaunchConfiguration('frame_id', default='laser')
     inverted = LaunchConfiguration('inverted', default='false')
     angle_compensate = LaunchConfiguration('angle_compensate', default='true')
-  
+
     publish_tf = LaunchConfiguration('publish_tf', default='true')
-  
+
     # Specify the name of the package and path to xacro file within the package
     pkg_name = 'urdf_example'
     file_subpath = 'description/example_robot.urdf.xacro'
 
     # Use xacro to process the file
-    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toxml()  
-    
+    xacro_file = os.path.join(
+        get_package_share_directory(pkg_name), file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
     return LaunchDescription([
 
         DeclareLaunchArgument(
@@ -42,7 +44,7 @@ def generate_launch_description():
             'serial_baudrate',
             default_value=serial_baudrate,
             description='Specifying usb port baudrate to connected lidar'),
-        
+
         DeclareLaunchArgument(
             'frame_id',
             default_value=frame_id,
@@ -57,51 +59,59 @@ def generate_launch_description():
             'angle_compensate',
             default_value=angle_compensate,
             description='Specifying whether or not to enable angle_compensate of scan data'),
-        
+
         Node(
             package='sllidar_ros2',
             executable='sllidar_node',
             name='sllidar_node',
-            parameters=[{'serial_port': serial_port, 
-                         'serial_baudrate': serial_baudrate, 
+            parameters=[{'serial_port': serial_port,
+                         'serial_baudrate': serial_baudrate,
                          'frame_id': frame_id,
-                         'inverted': inverted, 
+                         'inverted': inverted,
                          'angle_compensate': angle_compensate}],
             output='screen'),
-            
+
         Node(
             package="tf2_ros",
             executable="static_transform_publisher",
-            output="screen" ,
+            output="screen",
             arguments=["0", "0", "0", "0", "0", "0", "lidar_link", "laser"]
         ),
-        
+
         Node(
             package="tf2_ros",
             executable="static_transform_publisher",
-            output="screen" ,
+            output="screen",
             arguments=["0", "0", "0", "0", "0", "0", "base_link", "lidar_link"]
         ),
-        
-       #         Node(
-       #     package="tf2_ros",
-       #     executable="static_transform_publisher",
-       #     output="screen" ,
-       #     arguments=["0", "0", "0", "0", "0", "0", "odom", "base_footprint"]
-       # ),
-            
+
+
+        # Node(
+        #    package='ros2_laser_scan_matcher',
+        #    executable='laser_scan_matcher',
+        #    parameters=[{'publish_odom':'odom',
+        #                 'publish_tf': publish_tf,
+        #                 'base_frame':'base_footprint'}]),
+
         Node(
-            package='ros2_laser_scan_matcher',
-            executable='laser_scan_matcher',
-            parameters=[{'publish_odom':'odom',
-                         'publish_tf': publish_tf,
-                         'base_frame':'base_footprint'}]),
-        
+            package='rf2o_laser_odometry',
+            executable='rf2o_laser_odometry_node',
+            name='rf2o_laser_odometry',
+            output='screen',
+            parameters=[{
+                    'laser_scan_topic': '/scan',
+                    'odom_topic': '/odom',
+                    'publish_tf': True,
+                    'base_frame_id': 'base_footprint',
+                    'odom_frame_id': 'odom',
+                    'init_pose_from_topic': '',
+                    'freq': 10.0}],
+        ),
+
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
             parameters=[{'robot_description': robot_description_raw}]),
-            
-    ])
 
+    ])
