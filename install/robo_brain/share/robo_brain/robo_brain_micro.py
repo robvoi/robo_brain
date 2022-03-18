ef37c3ab73f1b23dc50c97be42c91950312f1743
Robert Voigtlaender <r.voigtlaender@gmail.com>
2022-03-18 11:03:56 +0100

    added micro launch

---
+++ install/robo_brain/share/robo_brain/robo_brain_micro.py
@@ -0,0 +1,136 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
+from nav2_common.launch import RewrittenYaml
+import xacro
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration(
+        'serial_baudrate', default='115200')  # for A1/A2 is 115200
+    frame_id = LaunchConfiguration('frame_id', default='laser')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+
+    publish_tf = LaunchConfiguration('publish_tf', default='true')
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'urdf_example'
+    file_subpath = '/home/robo/robo_ws/robo_brain/urdf/example_robot.urdf.xacro'
+    # Use xacro to process the file
+    xacro_file = os.path.join(file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    # bringup package
+    bringup_dir = get_package_share_directory('nav2_bringup')
+
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    autostart = LaunchConfiguration('autostart')
+    params_file = LaunchConfiguration('params_file')
+
+    lifecycle_nodes = ['controller_server',
+                       'planner_server',
+                       'recoveries_server',
+                       'bt_navigator',
+                       'waypoint_follower']
+
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'autostart': autostart}
+
+    configured_params = RewrittenYaml(
+        source_file=params_file,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+
+    # bringup end
+
+    return LaunchDescription([
+        # bringup start
+        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+
+        DeclareLaunchArgument(
+            'namespace', default_value='',
+            description='Top-level namespace'),
+
+        DeclareLaunchArgument(
+            'use_sim_time', default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        DeclareLaunchArgument(
+            'autostart', default_value='true',
+            description='Automatically startup the nav2 stack'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=os.path.join(
+                bringup_dir, 'params', 'nav2_params.yaml'),
+            description='Full path to the ROS2 parameters file to use'),
+
+        # bringup end
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Dont use simulation/Gazebo clock'),
+
+        DeclareLaunchArgument(
+            'slam_params_file',
+            default_value=os.path.join(get_package_share_directory("slam_toolbox"),
+                                       'config', 'mapper_params_online_async.yaml'),
+            description='Full path to the ROS2 parameters file to use for the slam_toolbox node'),
+
+        DeclareLaunchArgument(
+            'publish_tf',
+            default_value=publish_tf,
+            description='publish TD ODOM Base Link'),
+
+        DeclareLaunchArgument(
+            'serial_port',
+            default_value=serial_port,
+            description='Specifying usb port to connected lidar'),
+
+        DeclareLaunchArgument(
+            'serial_baudrate',
+            default_value=serial_baudrate,
+            description='Specifying usb port baudrate to connected lidar'),
+
+        DeclareLaunchArgument(
+            'frame_id',
+            default_value=frame_id,
+            description='Specifying frame_id of lidar'),
+
+        DeclareLaunchArgument(
+            'inverted',
+            default_value=inverted,
+            description='Specifying whether or not to invert scan data'),
+
+        DeclareLaunchArgument(
+            'angle_compensate',
+            default_value=angle_compensate,
+            description='Specifying whether or not to enable angle_compensate of scan data'),
+
+
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            arguments=["serial", "-b", "-D", "/dev/ttyACM0"]
+        )
+
+    ])
