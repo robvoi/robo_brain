7bbe4fb59ccbc3334ceced610b3c149c433bd0c1
Robert Voigtlaender <r.voigtlaender@gmail.com>
2022-03-20 15:35:32 +0100

    -

--- install/robo_brain/share/robo_brain/robo_brain_full.py
+++ install/robo_brain/share/robo_brain/robo_brain_full.py
@@ -11,31 +11,10 @@ import xacro
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
     slam_params_file = LaunchConfiguration('slam_params_file')
-
-    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
-    serial_baudrate = LaunchConfiguration(
-        'serial_baudrate', default='115200')  # for A1/A2 is 115200
-    frame_id = LaunchConfiguration('frame_id', default='laser')
-    inverted = LaunchConfiguration('inverted', default='false')
-    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
-
-    publish_tf = LaunchConfiguration('publish_tf', default='true')
-
-    # Specify the name of the package and path to xacro file within the package
-    pkg_name = 'urdf_example'
-    file_subpath = '/home/robo/robo_ws/robo_brain/urdf/example_robot.urdf.xacro'
-    # Use xacro to process the file
-    xacro_file = os.path.join(file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
-
     # bringup package
     bringup_dir = get_package_share_directory('nav2_bringup')
 
-    namespace = LaunchConfiguration('namespace')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    autostart = LaunchConfiguration('autostart')
     params_file = LaunchConfiguration('params_file')
 
     lifecycle_nodes = ['controller_server',
@@ -48,13 +27,9 @@ def generate_launch_description():
                   ('/tf_static', 'tf_static')]
 
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'autostart': autostart}
-
+    param_substitutions = {}
     configured_params = RewrittenYaml(
         source_file=params_file,
-        root_key=namespace,
         param_rewrites=param_substitutions,
         convert_types=True)
 
@@ -65,66 +40,16 @@ def generate_launch_description():
         SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
 
         DeclareLaunchArgument(
-            'namespace', default_value='',
-            description='Top-level namespace'),
-
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        DeclareLaunchArgument(
-            'autostart', default_value='true',
-            description='Automatically startup the nav2 stack'),
-
-        DeclareLaunchArgument(
             'params_file',
-            default_value=os.path.join(
-                bringup_dir, 'params', 'nav2_params.yaml'),
+            default_value='/home/robo/robo_ws/robo_brain/nav2_config/nav2_params.yaml',
             description='Full path to the ROS2 parameters file to use'),
 
         # bringup end
 
         DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Dont use simulation/Gazebo clock'),
-
-        DeclareLaunchArgument(
             'slam_params_file',
-            default_value=os.path.join(get_package_share_directory("slam_toolbox"),
-                                       'config', 'mapper_params_online_async.yaml'),
+            default_value='/home/robo/robo_ws/robo_brain/nav2_config/mapper_params_online_async.yaml',
             description='Full path to the ROS2 parameters file to use for the slam_toolbox node'),
-
-        DeclareLaunchArgument(
-            'publish_tf',
-            default_value=publish_tf,
-            description='publish TD ODOM Base Link'),
-
-        DeclareLaunchArgument(
-            'serial_port',
-            default_value=serial_port,
-            description='Specifying usb port to connected lidar'),
-
-        DeclareLaunchArgument(
-            'serial_baudrate',
-            default_value=serial_baudrate,
-            description='Specifying usb port baudrate to connected lidar'),
-
-        DeclareLaunchArgument(
-            'frame_id',
-            default_value=frame_id,
-            description='Specifying frame_id of lidar'),
-
-        DeclareLaunchArgument(
-            'inverted',
-            default_value=inverted,
-            description='Specifying whether or not to invert scan data'),
-
-        DeclareLaunchArgument(
-            'angle_compensate',
-            default_value=angle_compensate,
-            description='Specifying whether or not to enable angle_compensate of scan data'),
-
 
         Node(
             package='micro_ros_agent',
@@ -137,20 +62,13 @@ def generate_launch_description():
             package='sllidar_ros2',
             executable='sllidar_node',
             name='sllidar_node',
-            parameters=[{'serial_port': serial_port,
-                         'serial_baudrate': serial_baudrate,
-                         'frame_id': frame_id,
-                         'inverted': inverted,
-                         'angle_compensate': angle_compensate, }],
+            parameters=[{'serial_port': '/dev/ttyUSB0',
+                         'serial_baudrate': 115200,
+                         'frame_id': 'laser',
+                         'inverted': False,
+                         'angle_compensate': True, }],
             output='screen',
             arguments=['--ros-args', '--log-level', 'WARN']),
-
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            output="screen",
-            arguments=["0", "0", "0", "0", "0", "0", "lidar_link", "laser"]
-        ),
 
         Node(
             package='rf2o_laser_odometry',
@@ -172,13 +90,14 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_description_raw}],
-            arguments=[file_subpath]),
+            parameters=[{'robot_description': xacro.process_file(
+                '/home/robo/robo_ws/robo_brain/urdf/robot.urdf.xacro').toxml()}],
+            arguments=['/home/robo/robo_ws/robo_brain/urdf/robot.urdf.xacro']),
 
         Node(
             parameters=[
                 slam_params_file,
-                {'use_sim_time': use_sim_time}
+                {'use_sim_time': False}
             ],
             package='slam_toolbox',
             executable='async_slam_toolbox_node',
@@ -229,8 +148,8 @@ def generate_launch_description():
             executable='lifecycle_manager',
             name='lifecycle_manager_navigation',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
+            parameters=[{'use_sim_time': False},
+                        {'autostart': True},
                         {'node_names': lifecycle_nodes}])
         # bringup end
 
--- install/robo_brain/share/robo_brain/robo_brain_full_test.py
+++ install/robo_brain/share/robo_brain/robo_brain_full_test.py
@@ -11,31 +11,10 @@ import xacro
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
     slam_params_file = LaunchConfiguration('slam_params_file')
-
-    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
-    serial_baudrate = LaunchConfiguration(
-        'serial_baudrate', default='115200')  # for A1/A2 is 115200
-    frame_id = LaunchConfiguration('frame_id', default='laser')
-    inverted = LaunchConfiguration('inverted', default='false')
-    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
-
-    #publish_tf = LaunchConfiguration('publish_tf', default='true')
-
-    # Specify the name of the package and path to xacro file within the package
-    pkg_name = 'urdf_example'
-    file_subpath = '/home/robo/robo_ws/robo_brain/urdf/example_robot.urdf.xacro'
-    # Use xacro to process the file
-    xacro_file = os.path.join(file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
-
     # bringup package
     bringup_dir = get_package_share_directory('nav2_bringup')
 
-    namespace = LaunchConfiguration('namespace')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    autostart = LaunchConfiguration('autostart')
     params_file = LaunchConfiguration('params_file')
 
     lifecycle_nodes = ['controller_server',
@@ -48,13 +27,9 @@ def generate_launch_description():
                   ('/tf_static', 'tf_static')]
 
     # Create our own temporary YAML files that include substitutions
-    param_substitutions = {
-        'use_sim_time': use_sim_time,
-        'autostart': autostart}
-
+    param_substitutions = {}
     configured_params = RewrittenYaml(
         source_file=params_file,
-        root_key=namespace,
         param_rewrites=param_substitutions,
         convert_types=True)
 
@@ -65,78 +40,33 @@ def generate_launch_description():
         SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
 
         DeclareLaunchArgument(
-            'namespace', default_value='',
-            description='Top-level namespace'),
-
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        DeclareLaunchArgument(
-            'autostart', default_value='true',
-            description='Automatically startup the nav2 stack'),
-
-        DeclareLaunchArgument(
             'params_file',
-            default_value=os.path.join(
-                bringup_dir, 'params', 'nav2_params.yaml'),
+            default_value='/home/robo/robo_ws/robo_brain/nav2_config/nav2_params.yaml',
             description='Full path to the ROS2 parameters file to use'),
 
         # bringup end
 
         DeclareLaunchArgument(
             'slam_params_file',
-            default_value=os.path.join(get_package_share_directory("slam_toolbox"),
-                                       'config', 'mapper_params_online_async.yaml'),
+            default_value='/home/robo/robo_ws/robo_brain/nav2_config/mapper_params_online_async.yaml',
             description='Full path to the ROS2 parameters file to use for the slam_toolbox node'),
 
-        #DeclareLaunchArgument(
-        #    'publish_tf',
-        #    default_value=publish_tf,
-        #    description='publish TD ODOM Base Link'),
-
-        DeclareLaunchArgument(
-            'serial_port',
-            default_value=serial_port,
-            description='Specifying usb port to connected lidar'),
-
-        DeclareLaunchArgument(
-            'serial_baudrate',
-            default_value=serial_baudrate,
-            description='Specifying usb port baudrate to connected lidar'),
-
-        DeclareLaunchArgument(
-            'frame_id',
-            default_value=frame_id,
-            description='Specifying frame_id of lidar'),
-
-        DeclareLaunchArgument(
-            'inverted',
-            default_value=inverted,
-            description='Specifying whether or not to invert scan data'),
-
-        DeclareLaunchArgument(
-            'angle_compensate',
-            default_value=angle_compensate,
-            description='Specifying whether or not to enable angle_compensate of scan data'),
-
-
-        #Node(
+        # Node(
         #    package='micro_ros_agent',
         #    executable='micro_ros_agent',
         #    name='micro_ros_agent',
         #    arguments=["serial", "-b", "230400", "-D", "/dev/ttyACM0"]
-        #),
+        # ),
 
         Node(
             package='sllidar_ros2',
             executable='sllidar_node',
             name='sllidar_node',
-            parameters=[{'serial_port': serial_port,
-                         'serial_baudrate': serial_baudrate,
-                         'frame_id': frame_id,
-                         'inverted': inverted,
-                         'angle_compensate': angle_compensate, }],
+            parameters=[{'serial_port': '/dev/ttyUSB0',
+                         'serial_baudrate': 115200,
+                         'frame_id': 'laser',
+                         'inverted': False,
+                         'angle_compensate': True, }],
             output='screen',
             arguments=['--ros-args', '--log-level', 'WARN']),
 
@@ -160,13 +90,14 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_description_raw}],
-            arguments=[file_subpath]),
+            parameters=[{'robot_description': xacro.process_file(
+                '/home/robo/robo_ws/robo_brain/urdf/robot.urdf.xacro').toxml()}],
+            arguments=['/home/robo/robo_ws/robo_brain/urdf/robot.urdf.xacro']),
 
         Node(
             parameters=[
                 slam_params_file,
-                {'use_sim_time': use_sim_time}
+                {'use_sim_time': False}
             ],
             package='slam_toolbox',
             executable='async_slam_toolbox_node',
@@ -217,8 +148,8 @@ def generate_launch_description():
             executable='lifecycle_manager',
             name='lifecycle_manager_navigation',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
+            parameters=[{'use_sim_time': False},
+                        {'autostart': True},
                         {'node_names': lifecycle_nodes}])
         # bringup end
 
